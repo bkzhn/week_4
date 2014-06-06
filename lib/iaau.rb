c34101b20f428d070bdf0ae1096782adc019cb1f
bkzhn <absh.bktl@gmail.com>
2014-06-06 06:28:20 +0600

    Update week_4

--- lib/iaau.rb
+++ lib/iaau.rb
@@ -6,6 +6,26 @@
 
 class IAAU
   def self.iaau
+=begin
+	(string) -> (string)
+
+	this method has to accept argument
+	of string type and pass it to ModeCheck class
+	which will return specific string
+
+	>  IAAU.iaau("full")
+	=> "International Ataturk Alatoo University"
+
+	>  IAAU.iaau("short")
+	=> "IAAU"
+
+	>  IAAU.iaau()
+	=> "IAAU"
+
+	>  IAAU.iaau("laskdjf")
+	=> "Unkown mode"
+=end
+	# write your code here
   end
 end
 
--- lib/iaau/modecheck.rb
+++ lib/iaau/modecheck.rb
@@ -1,4 +1,24 @@
 class ModeCheck
   def iaau
+=begin
+	(string) -> (string)
+
+	this method has to accept argument
+	of string type, check it and
+	 return specific string
+
+	>  iaau("full")
+	=> "International Ataturk Alatoo University"
+
+	>  iaau("short")
+	=> "IAAU"
+
+	>  iaau()
+	=> "IAAU"
+
+	>  iaau("laskdjf")
+	=> "Unkown mode"
+=end
+	# write your code here
   end
 end
